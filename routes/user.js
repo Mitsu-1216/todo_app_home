3586e3b2b28248aa18dfed971570ff6bf9313ea4
MITSU-1216 <adieuer._.03.26@icloud.com>
2023-04-25 21:45:24 +0900

    user_display

--- routes/user.js
+++ routes/user.js
@@ -5,19 +5,13 @@ const knex = require("../db/knex");
 
 //select文を書く
 router.get('/', function (req, res, next) {
-    const userId = req.session.userid;
-    const isAuth = Boolean(userId);
+
     res.render('user', {
         title: 'user★',
-        isAuth: isAuth
     });
 });
 
 router.post('/', function (req, res, next) {
-    const userId = req.session.userid;
-    const isAuth = Boolean(userId);
-    const username = req.body.username;
-    const password = req.body.password;
 
     knex("users")
         .select("*")
@@ -25,15 +19,13 @@ router.post('/', function (req, res, next) {
             res.render("user", {
                 title: "user",
                 results:results,
-                isAuth: isAuth,
             });        
         })
         .catch(function (err) {
             console.error(err);
             res.render("user", {
                 title: "user",
-                isAuth: isAuth,
-                errorMessage: [err.sqlMessage],
+               errorMessage: [err.sqlMessage],
             });
         });
 });
