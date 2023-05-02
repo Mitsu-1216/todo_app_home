470cf7f22fed22e664a392157a68fb4efb5b380d
MITSU-1216 <adieuer._.03.26@icloud.com>
2023-05-03 04:43:37 +0900

    users

--- routes/user.js
+++ routes/user.js
@@ -12,20 +12,38 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', function (req, res, next) {
+    const userId = req.session.userid;
+    const isAuth = Boolean(userId);
+    let admin_flg = '';
 
     knex("users")
         .select("*")
         .then((results) => {
+            console.log('userId:' + userId);
+            console.log('results[0].id:' + results[0].id);
+            // userの数が取得できない
+            // for文で回すより、findなどで取得したオブジェクトにuseridが含まれるか調べる
+
+            for (let i = 0; i < results.lenght; i++) {
+                if (userId === results[i].id) {
+                    admin_flg = results[i].admin_flg;
+            console.log('admin_flg1:' + admin_flg);
+                }
+            console.log('admin_flg2:' + admin_flg);
+             }
+
+            console.log('admin_flg3:' + admin_flg);
             res.render("user", {
                 title: "user",
-                results:results,
-            });        
+                results: results,
+                admin_flg: admin_flg
+            });
         })
         .catch(function (err) {
             console.error(err);
             res.render("user", {
                 title: "user",
-               errorMessage: [err.sqlMessage],
+                errorMessage: [err.sqlMessage],
             });
         });
 });
